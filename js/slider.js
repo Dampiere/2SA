4e4bf126536927bf09365e65ca38f24268d2cfa7
Umanskyi Alexander <44087027+Dampiere@users.noreply.github.com>
2021-05-20 15:11:08 +0300

    test

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.main__slider').slick({
+        variableWidth: true,
+    });
+});
+
+$(document).ready(function(){
+    $('.interface__content__slider__items').slick({
+        variableWidth: true,
+    });
+});
